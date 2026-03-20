233abd785465289a4c5e0392a4780b1ca72ca80c
kostjakuris <kostjn04e@gmail.com>
2026-03-20 12:10:27 +0200

    fix websocket connection

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -4,7 +4,12 @@ let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_BACKEND_URL);
+    socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
+      transports: ['websocket'],
+      withCredentials: true,
+      secure: true,
+      path: '/socket.io',
+    });
   }
   return socket;
 };
